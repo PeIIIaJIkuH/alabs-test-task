2612b7ce2e24f9193a55cf41129d18a29a682230
PeIIIaJIkuH <baigal.erhan@gmail.com>
2021-04-29 16:56:21 +0600

    update: added animation to AddItem component

--- src/components/AddItem/AddItem.tsx
+++ src/components/AddItem/AddItem.tsx
@@ -14,6 +14,7 @@ interface AddItemProps {
 export const AddItem: FC<AddItemProps> = ({text, type, column}) => {
 	const [name, setName] = useState(''),
 		[isEditMode, setIsEditMode] = useState(false),
+		[isAnimated, setIsAnimated] = useState(false),
 		inputRef = useRef<HTMLInputElement>(null),
 		createDivRef = useRef<HTMLDivElement>(null)
 
@@ -23,11 +24,15 @@ export const AddItem: FC<AddItemProps> = ({text, type, column}) => {
 
 	const handleOnAddClick = () => {
 		setIsEditMode(true)
+		setIsAnimated(true)
 	}
 
 	const closeInput = () => {
-		setIsEditMode(false)
-		setName('')
+		setIsAnimated(false)
+		setTimeout(() => {
+			setName('')
+			setIsEditMode(false)
+		}, 200)
 	}
 
 	const handleOnCancelClick = () => {
@@ -67,13 +72,14 @@ export const AddItem: FC<AddItemProps> = ({text, type, column}) => {
 	})
 
 	return (
-		<div className={`${type === 'column' ? s.addColumnDiv : s.addTodoDiv}`}>
+		<div className={`${s.addDiv} ${isEditMode && s.expandAddDiv} ${type === 'column' ? s.addColumnDiv : s.addTodoDiv}`}>
 			{!isEditMode ? (
-				<button className={`${s.addBtn} ${type === 'column' ? s.addColumnBtn : s.addTodoBtn}`} onClick={handleOnAddClick}>
+				<button className={`${s.addBtn} ${isAnimated ? s.hideAddBtn : s.showAddBtn} ${type === 'column' ? s.addColumnBtn : s.addTodoBtn}`}
+						onClick={handleOnAddClick}>
 					{text}
 				</button>
 			) : (
-				<div className={s.createDiv} ref={createDivRef}>
+				<div className={`${s.createDiv} ${isAnimated ? s.showCreateDiv : s.hideCreateDiv}`} ref={createDivRef}>
 					<input autoFocus className={s.input} type='text' value={name} onChange={handleOnInputChange} onKeyPress={handleOnInputKeyPress}
 						   onKeyDown={handleOnInputKeyDown} ref={inputRef}/>
 					<div className={s.actions}>
